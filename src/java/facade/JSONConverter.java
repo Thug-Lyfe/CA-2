a8b1ab92d92db8a971cc845025b6757d79ca93ee
Invictus420 <alexandar_slambert@hotmail.com>
2016-03-16 14:42:48 +0100

    api

--- src/java/facade/JSONConverter.java
+++ src/java/facade/JSONConverter.java
@@ -7,6 +7,9 @@ package facade;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import entity.Person;
 import java.util.List;
 
@@ -19,10 +22,32 @@ public class JSONConverter {
     private static Gson gson = new GsonBuilder().setPrettyPrinting().create();
     
     public static String getJSONFromPerson(Person p){
-        return gson.toJson(p);
+        JsonObject person = new JsonObject();
+        person.addProperty("id", p.getId());
+        person.addProperty("firstname", p.getFirstName());
+        person.addProperty("lastname", p.getLastName());
+        person.addProperty("email", p.getEmail());
+        person.addProperty("street", p.getHood().getStreet());
+        person.addProperty("addinfo", p.getHood().getAdditonalInfo());
+        for (int i = 0; i < p.getPhonies().size(); i++) {
+            person.addProperty("phonenumber " + i, p.getPhonies().get(i).getNumber());
+            person.addProperty("phonedescription" + i, p.getPhonies().get(i).getDisc());
+        }
+        person.addProperty("zipcode", p.getHood().getShityInfo().getZipCode());
+        person.addProperty("city", p.getHood().getShityInfo().getCity());
+        for (int i = 0; i < p.getHobbies().size(); i++) {
+            person.addProperty("hobby " +i, p.getHobbies().get(i).getName());
+            person.addProperty("hobbydescription " +i, p.getHobbies().get(i).getDisc());
+        }
+        return gson.toJson(person);
     } 
     public static String getJSONFromPerson(List<Person> list){
-        return gson.toJson(list);
+        JsonArray people = new JsonArray();
+        for (Person p : list) {
+            people.add(getJSONFromPerson(p));
+        }
+        
+        return gson.toJson(people);
     }
     
     
